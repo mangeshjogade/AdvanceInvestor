7fa3708436fb42572879077559ffb61a5fa44020
Mangesh Jogade <mangesh.mpj@gmail.com>
2017-08-15 19:46:52 -0500

    Added Agent spring boot project

---
+++ advance-investor-agent/src/main/java/com/advanceinvestor/agent/AdvanceInvestorAgentApplication.java
@@ -0,0 +1,12 @@
+package com.advanceinvestor.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceInvestorAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceInvestorAgentApplication.class, args);
+	}
+}
---
+++ advance-investor-agent/src/test/java/com/advanceinvestor/agent/AdvanceInvestorAgentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.advanceinvestor.agent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdvanceInvestorAgentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
